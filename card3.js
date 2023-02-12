2c46561c76dbb8dd9ab960b059793ac7a89c0ffa
zain00abd <124315644+zain00abd@users.noreply.github.com>
2023-02-12 21:12:02 +0200

    Add files via upload

--- card3.js
+++ card3.js
@@ -94,8 +94,8 @@ textobj.innerHTML = '----'
 
 function getOS() {
     
-    // var userAgent = window.navigator.userAgent
-    // var platform = window.navigator.userAgentData.platform || window.navigator.platform
+    var userAgent = window.navigator.userAgent
+    var platform = window.navigator.userAgentData.platform || window.navigator.platform
     var macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K']
     var windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE']
     var iosPlatforms = ['iPhone', 'iPad', 'iPod']
@@ -114,7 +114,6 @@ function getOS() {
     
     return os;
 }
-getOS()
 
 function zoom(){
     if(os === 'Android' || os === 'iOS'){
